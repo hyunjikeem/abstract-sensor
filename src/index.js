ac6d452566bfc677ef03637c1dd3526d069c24d2
HyunjiKim818 <kjhxxxx@gmail.com>
2022-02-07 21:24:15 +0900

    test: 기본 테스트 코드 작성

--- src/index.js
+++ src/index.js
@@ -1,6 +1,51 @@
-class Sensor {}
+class Sensor {
+    constructor(id) {
+        this.is = id;
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+    }
 
-class IotServer {}
+    turn(status) {
+        if (this.powerStatus === 'off' && status === 'on') {
+            this.powerStatus = 'on';
+            this.status = 'idle';
+            
+            setTimeout (() => {
+            this.status = 'sensingDistance';
+        }, this.reportingInterval);
+        setTimeout (() => {
+            this.status = 'reportingData';
+        }, this.reportingInterval + 500);
+        setTimeout (() => {
+            this.status = 'idle';
+        }, this.reportingInterval + 1000);
+        } else if (status === 'off') {
+            this.powerStatus = 'off';
+        } else {
+            throw new Error ('server not changed');
+        }
+    }
+}
+
+class IotServer {
+    constructor() {
+        this.sensor = [];
+    }
+
+    start([sensor]) {
+        this.sensor.push(sensor);
+    }
+
+    publish({ devideId, actionId, payload }) {
+        for (let i = 0; i < this.sensor.length; i++) {
+            if (this.sensor[i].id === devideId) {
+                if (this.sensor[i].powerStatus === 'on' && actionId === 'CHANGE_REPORTING_INTERVAL') {
+                    this.sensor[i].reportingInterval = payload;
+                };
+            };
+        };
+    };
+};
 
 module.exports = {
     Sensor,
